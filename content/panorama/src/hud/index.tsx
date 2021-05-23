8e734f181c8c1cef4f0406197a32ceb2cd4440ac
Martin Tsai <cauu@163.com>
2021-05-23 22:00:31 +0800

    feat: 完善boilplate

--- content/panorama/src/hud/index.tsx
+++ content/panorama/src/hud/index.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import { render } from 'react-panorama';
 
+import { enableDefaultUI } from '../utils/ui'
+
+import './style.less'
+
+enableDefaultUI()
+
 function Counter() {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count + 1);
 
   return (
     <Panel style={{ flowChildren: 'down' }}>
-      <Label text={`Count: ${count}`} />
+      <Label className="name" text={`count: ${count}`} />
       <TextButton className="ButtonBevel" text="Increment" onactivate={increment} />
     </Panel>
   );
---
+++ content/panorama/src/utils/ui.ts
@@ -0,0 +1,19 @@
+export function disableDefaultUI () {
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_ACTION_PANEL, false);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_ACTION_MINIMAP, true);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_PANEL, false);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_SHOP, false);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_FLYOUT_SCOREBOARD, false);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_BAR_BACKGROUND, false);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_HEROES, false);
+}
+
+export function enableDefaultUI () {
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_ACTION_PANEL, true);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_ACTION_MINIMAP, true);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_PANEL, true);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_INVENTORY_SHOP, true);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_FLYOUT_SCOREBOARD, true);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_BAR_BACKGROUND, true);
+  GameUI.SetDefaultUIEnabled(DotaDefaultUIElement_t.DOTA_DEFAULT_UI_TOP_HEROES, true);
+}
