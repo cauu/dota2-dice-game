227e96b892edf751fd58765856c6afba837480b9
Martin Tsai <cauu@163.com>
2021-07-04 00:31:38 +0800

    feat: 更新英雄选择洁面

--- content/panorama/src/hud/layouts/HeroSelectionLayout/index.tsx
+++ content/panorama/src/hud/layouts/HeroSelectionLayout/index.tsx
@@ -2,35 +2,55 @@ import React from 'react'
 
 import './style.less'
 
-// const HERO_LIST = [
-//   {
-//     id: 'npc_dota_hero_antimage',
-//     img: 'file://{images}/loadingscreens/witch_hunter_loading_screen/loadingscreen.tga'
-//   },
-//   {
-//     id: 'npc_dota_hero_legion_commander',
-//     img: 'file://{images}/loadingscreens/valkyrie_loadingscreen/loadingscreen.tga'
-//   },
-//   {
-//     id: 'npc_dota_hero_drow_ranger',
-//     img: 'file://{images}/loadingscreens/traxex_loadingscreen/loadingscreen.tga'
-//   },
-//   {
-//     id: 'npc_dota_hero_rubick',
-//     img: 'file://{images}/loadingscreens/harlequin_loading_screen/loadingscreen.tga'
-//   }
-// ]
+const HERO_LIST = [
+  {
+    id: 'npc_dota_hero_antimage',
+    img: 'url("file://{images}/loadingscreens/witch_hunter_loading_screen/loadingscreen.tga")'
+  },
+  {
+    id: 'npc_dota_hero_legion_commander',
+    img: 'url("file://{images}/loadingscreens/valkyrie_loadingscreen/loadingscreen.tga")'
+  },
+  {
+    id: 'npc_dota_hero_drow_ranger',
+    img: 'url("file://{images}/loadingscreens/traxex_loadingscreen/loadingscreen.tga")'
+  },
+  {
+    id: 'npc_dota_hero_rubick',
+    img: 'url("file://{images}/loadingscreens/harlequin_loading_screen/loadingscreen.tga")'
+  }
+]
 
 function HeroSelectionLayout () {
   return (
     <Panel className="hero-selection">
+      <Panel className="hero-selection__left">
+      </Panel>
+
       <Panel className="hero-selection__main">
-        <Panel className="hero-selection__timer">
-          {/* <Label id='TimerTxt' text="" /> */}
+        <Panel className="hero-selection__text">
+          <Panel className="hero-selection__timer">
+            <Label id='timerText' text="" />
+          </Panel>
+
+          <Label id="heroSelectedText" text="Select a hero" />
         </Panel>
 
         <Panel className="hero-selection__list">
+          {
+            HERO_LIST.map((hero) => {
+              return (
+                <Panel key={hero.id}
+                  className="hero-selection__list-item" 
+                  style={{ backgroundImage: hero.img }}
+                />
+              )
+            })
+          }
         </Panel>
+      </Panel>
+
+      <Panel className="hero-selection__right">
       </Panel>
 
     </Panel>
